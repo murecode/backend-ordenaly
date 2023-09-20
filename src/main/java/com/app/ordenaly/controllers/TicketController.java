8fffc152acca89a8bdf61a4ef7f925c4ecd77cbb
muredev <jerico9222@hotmail.com>
2023-09-20 13:51:15 -0500

    srv y ctr: listar todos los tickets

--- src/main/java/com/app/ordenaly/controllers/TicketController.java
+++ src/main/java/com/app/ordenaly/controllers/TicketController.java
@@ -7,16 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api/tickets")
 public class TicketController {
   @Autowired
   TicketService ticketService;
 
-  @GetMapping(value = "/new")
+  @GetMapping("/list")
+  public List<Ticket> listAllTickets() {
+    return ticketService.getAllTickets();
+  }
+
+  @GetMapping("/new")
   public Ticket newTicket(Ticket ticket) {
     return ticketService.generateTicket(ticket);
-
   }
 
 }
--- src/main/java/com/app/ordenaly/services/TicketService.java
+++ src/main/java/com/app/ordenaly/services/TicketService.java
@@ -5,6 +5,8 @@ import com.app.ordenaly.repositories.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TicketService {
   @Autowired
@@ -12,6 +14,10 @@ public class TicketService {
 
   public Ticket generateTicket(Ticket ticket) {
     return ticketRepo.save(ticket);
+  }
+
+  public List<Ticket> getAllTickets() {
+    return ticketRepo.findAll();
   }
 
 }
